d50c95ca56ec4c6515e0ab9244c243e13e88b6b9
Suhardi100 <59378320+Suhardi100@users.noreply.github.com>
2025-11-12 22:05:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ os.environ["LANGCHAIN_PROJECT"] = "UU-CiptaKerja-AgenticRAG"
 # 🔮 Setup Google Gemini
 # ================================
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash-lite",
+    model="gemini-2.0-flash",
     temperature=0.1,
     google_api_key="AIzaSyBolTu5dpLti8ndnEOm0TLVxj2K2-FSeNQ"
 )
